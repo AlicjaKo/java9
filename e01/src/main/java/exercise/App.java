d96fc5f86cccb39e09394556ba1d04d19222cf64
Ala <ala@Alas-MacBook-Pro.local>
2024-02-07 19:47:28 +0200

    ex1

--- e01/src/main/java/exercise/App.java
+++ e01/src/main/java/exercise/App.java
@@ -1,4 +1,5 @@
 package exercise;
+import javax.swing.JOptionPane;
 
 public class App {
     private App() {
@@ -6,6 +7,39 @@ public class App {
     }
 
     public static void main(final String[] args) {
+        Traingle t = new Traingle(2,3);
+        Circle c = new Circle(3);
 
+        c.calculateArea();
+        t.calculateArea();
+    }
+}
+
+abstract class Shape {
+    calculateArea();
+}
+
+class Traingle extends Shape {
+    double base;
+    double height;
+
+    Triangle(double b, double h) {
+        this.base = b;
+        this.height = h;
+    }
+    calculateArea() {
+        System.out.println("Area: " + 0.5 * this.base * this.height);
+    }
+}
+
+class Circle extends Shape {
+    double radius;
+
+    Radius(double r) {
+        this.radius = r;
+    }
+
+    calculateArea() {
+        System.out.println("Area: " + Math.PI * this.radius * this.radius);
     }
 }
